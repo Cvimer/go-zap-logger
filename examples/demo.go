74ec16beef602b472a37213febb2ab3bd849cdc0
Cvimer <25924096+Cvimer@users.noreply.github.com>
2022-01-26 14:15:24 +0800

    Update demo.go

--- examples/demo.go
+++ examples/demo.go
@@ -5,9 +5,21 @@ import (
 )
 
 func main() {
+	InitLogger()
+
+	for i := 0; i < 20000; i++ {
+		log.Info("demo3:", log.String("app", "start ok"),
+			log.Int("major version", 3))
+		log.Error("demo3:", log.String("app", "crash"),
+			log.Int("reason", -1))
+	}
+
+}
+
+func InitLogger() {
 	var tops = []log.TeeOption{
 		{
-			Filename: "access.log",
+			FileName: "access.log",
 			Ropt: log.RotateOptions{
 				MaxSize:    1,
 				MaxAge:     1,
@@ -19,7 +31,7 @@ func main() {
 			},
 		},
 		{
-			Filename: "error.log",
+			FileName: "error.log",
 			Ropt: log.RotateOptions{
 				MaxSize:    1,
 				MaxAge:     1,
@@ -31,15 +43,6 @@ func main() {
 			},
 		},
 	}
-
 	logger := log.NewTeeWithRotate(tops)
 	log.ResetDefault(logger)
-
-	for i := 0; i < 20000; i++ {
-		log.Info("demo3:", log.String("app", "start ok"),
-			log.Int("major version", 3))
-		log.Error("demo3:", log.String("app", "crash"),
-			log.Int("reason", -1))
-	}
-
 }
